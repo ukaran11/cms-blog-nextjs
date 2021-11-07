c372fb49db50f8b99e042144e11c7d79a05eecd5
Utsav Karan <ukaran11@gmail.com>
2021-11-07 21:54:25 +0530

    Utsav | Added static posts

--- pages/index.js
+++ pages/index.js
@@ -1,18 +1,28 @@
 import Head from 'next/head'
 
+const posts = [
+  {title: 'React Testing', excerpt: 'Learn React Testing'},
+  {title: 'React with Tailwind', excerpt: 'Learn React with Tailwind'},
+];
 
 export default function Home() {
   return (
     <>
-    <div className="container mx-auto px-10 mb-8">
-      <Head>
-        <title>CMS Blog</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
-    </div>
-    <div className='grid grid-cols-1 lg:grid-cols-12 gap-12s'>
-
+      <div className="container mx-auto px-10 mb-8 bg-gray-300">
+        <Head>
+          <title>CMS Blog</title>
+          <link rel="icon" href="/favicon.ico" />
+        </Head>
+        <div className='grid grid-cols-1 lg:grid-cols-12 gap-12'>
+        {posts.map((post, index) => (
+          <div>
+          {post.title}
+          {post.excerpt}
+          </div>
+          ))}
       </div>
-      </>
+      </div>
+      
+    </>
   )
 }
