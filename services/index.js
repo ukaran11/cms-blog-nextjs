32d1c4289fd2434032bf8ef1dc640055810bfe98
Utsav Karan <ukaran11@gmail.com>
2021-11-16 19:27:47 +0530

    Utsav | Added graphql query

--- services/index.js
+++ services/index.js
@@ -3,7 +3,34 @@ import { request, gql } from 'graphql-request';
 export const getPosts = async () => {
     const query = gql`
         query MyQuery {
-            
+            query MyQuery {
+                postsConnection {
+                    edges {
+                    node {
+                        author {
+                        bio
+                        name
+                        id
+                        photo {
+                            url
+                        }
+                        }
+                        createdAt
+                        slug
+                        title
+                        excerpt
+                        featuredImage {
+                        url
+                        }
+                        categories {
+                        name
+                        slug
+                        }
+                    }
+                    }
+                }
+                }
+
         }
     `
 }
