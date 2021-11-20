53ba9af980a10a0cb5da8c86a3ed41a34f495c21
Utsav Karan <ukaran11@gmail.com>
2021-11-20 19:14:38 +0530

    Utsav | Added image styling in Postcard

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -9,6 +9,8 @@ const PostCard = ({ post }) => {
             <div className="relative overflow-hidden shadow-md pb-80 mb-6">
                 <img 
                     src={post.featuredImage.url}
+                    alt={post.title}
+                    className="object-top absolute h-80 w-full object-cover shadow-lg rounded-t-lg lg:rounded-lg"
                 />
             </div>
         </div>
