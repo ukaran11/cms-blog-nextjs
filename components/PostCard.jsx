9885d079212aeb9d33ba3cbf815346874cdb6544
Utsav Karan <ukaran11@gmail.com>
2021-11-28 14:51:53 +0530

    Utsav | Added post link

--- components/PostCard.jsx
+++ components/PostCard.jsx
@@ -50,6 +50,13 @@ const PostCard = ({ post }) => {
         </div>
       </div>
       <p className="text-center text-lg text-gray-700 font-normal px-4 lg:px-20 mb-8">{post.excerpt}</p>
+      <div className="text-center">
+        <Link href={`/post/${post.slug}`}>
+          <span className="transition duration-500 transform hover:-translate-y-1 inline-block bg-pink-600 text-lg font-medium rounded-full text-white px-8 py-3 cursor-pointer">
+            Continiue Reading
+          </span>
+        </Link>
+      </div>
     </div>
   );
 };
